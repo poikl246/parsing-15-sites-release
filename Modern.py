18dd570ed4741415ae72e020e468d931179751b4
poikl246 <poikl.m246@gmail.com>
2022-01-04 22:52:13 +0500

    modern

--- Modern.py
+++ Modern.py
@@ -1,92 +1,96 @@
-import asyncio
 import os
-import random
-from fuzzywuzzy import fuzz
-import time
-from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 import requests
-from fake_useragent import UserAgent
-
-us = UserAgent()
-print(us.random)
-
-def parsing(data_master_scan_in, data_time=(time.time())):
-
-    url_list_output = []
-
-    output_data = []
-
-    async def fetch_url_data(session, url, caunt, data_master_scan):
-        print(url, caunt)
-        headers = {'Accept': '*/*', 'Connection': 'keep-alive',
-                   'User-Agent': f'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
-                   'Cache-Control': 'max-age=0', 'DNT': '1', 'Upgrade-Insecure-Requests': '1'}
-        try:
+from selenium import webdriver
+import time
+from multiprocessing import Pool
+import random
+from fuzzywuzzy import fuzz
 
 
-            async with session.get(url, headers=headers) as response:
-                resp = await response.text()
-                # print(resp)
-                soup = BeautifulSoup(resp, 'html.parser')
+# options
 
-                titul = soup.find("title").text
+url_list_output = []
 
-                # -----------------------------------------------------------------------------------
-                # Достать статью в переменную txt
+output_data = []
+options = webdriver.FirefoxOptions()
+
+# user-agent
+options.set_preference("general.useragent.override",
+                       "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
+
+# disable webdriver mode
+options.set_preference("dom.webdriver.enabled", True)
+
+
+# headless mode
+options.headless = False
 
 
 
-                tttt = ""
-                tttt = soup.find(class_='nw_imtxt clearfix')
-                txxt = ""
-                txxt = BeautifulSoup(str(tttt),"html.parser").findAll("p")
-                txt = ""
-                for i in txxt:
-                    txt+=i.text+"\n"
+def func_chunks_generators(seq, size):
+    return (seq[i::size] for i in range(size))
 
 
-                # ---------------------------------------Обработчик, можно не трогать----------------------------------------------
 
+# data_master_scan = []
+def get_data(url_list):
+    try:
+        driver = webdriver.Firefox(
+            executable_path=fr"{os.getcwd()}/geckodriver",
+            options=options
+        )
 
-                text_list = txt.lower().split(' ')
-                # print(text_list)
-                # caunt_local = 0
+        # "C:\\users\\selenium_python\\chromedriver\\chromedriver.exe"
+        # r"C:\users\selenium_python\chromedriver\chromedriver.exe"
+        for url, caunt, data_master_scan in url_list:
+            driver.get(url=url)
+            print(url)
+            time.sleep(0.5)
+            src = driver.page_source
+            soup = BeautifulSoup(src, 'html.parser')
+            titul = soup.find("title").text
+            txt = soup.find(class_="nwin_title").text.replace('\n', '') + '\n\n' + soup.find(class_="nw_imtxt clearfix").text.replace('\n', '')
+            print(txt)
+            text_list = txt.lower().split(' ')
+            # print(text_list)
+            # caunt_local = 0
+            exit_data = []
+            for one_line in data_master_scan:
+                caunt_local = 0
+                for twe in one_line:
+                    for master_text in text_list:
+                        if fuzz.ratio(master_text, twe) >= 80:
+                            caunt_local += 1
+                            break
 
-                exit_data = []
-                for one_line in data_master_scan:
-                    caunt_local = 0
-                    for twe in one_line:
-                        for master_text in text_list:
-                            if fuzz.ratio(master_text, twe) >= 85:
-                                caunt_local += 1
-                                break
+                if caunt_local == len(one_line):
+                    exit_data.append(1)
+                else:
+                    exit_data.append(0)
 
-                    if caunt_local == len(one_line):
-                        exit_data.append(1)
-                    else:
-                        exit_data.append(0)
+            print(exit_data, url)
 
-                print(exit_data, url)
+            # ******************************************************************************************************************************************
 
-                # ******************************************************************************************************************************************
+            # moderator меняем на название сайта
 
-                # moderator меняем на название сайта
+            if exit_data.count(1) != 0:
+                if os.listdir('files/Modern.az') == []:
+                    try:
+                        with open(f'files/Modern.az/text_{caunt}.txt', 'w', encoding='utf-8') as file:
+                            file.write(f'{url}\n\n{titul}\n\n{txt}')
+                        # caunt += 1
+                    except Exception as a:
+                        print(a)
+                    output_data.append([exit_data, url])
+                    # return [exit_data, url]
+                    # url_list_output.append(url)
 
-                if exit_data.count(1) != 0:
-                    if os.listdir('files/Modern.az') == []:
-                        try:
-                            with open(f'files/Modern.az/text_{caunt}.txt', 'w', encoding='utf-8') as file:
-                                file.write(f'{url}\n\n{titul}\n\n{txt}')
+                # ----------------------------------------------не трогать-------------------------------------------------------------
 
-                        except Exception as a:
-                            print(a)
-                        output_data.append([exit_data, url])
-
-                    # ----------------------------------------------не трогать-------------------------------------------------------------
-
-
-                    else:
+                else:
+                    try:
                         for dir_site in os.listdir('files'):
                             for dir_page in os.listdir(f'files/{dir_site}'):
                                 with open(f'files/{dir_site}/{dir_page}', 'r',encoding="utf-8") as file:
@@ -94,172 +98,162 @@ def parsing(data_master_scan_in, data_time=(time.time())):
                                     file.readline()
                                     file.readline()
                                     if fuzz.ratio(txt, file.read()) >= 50:
-                                        return 0
+                                        nsjrjrn=100/0
 
                         # ******************************************************************************************************************************************
 
-                        # moderator меняем на название сайта
+                        # musavat меняем на название сайта
 
                         try:
                             with open(f'files/Modern.az/text_{caunt}.txt', 'w', encoding='utf-8') as file:
                                 file.write(f'{url}\n\n{titul}\n\n{txt}')
                                 output_data.append([exit_data, url])
+                                # return [exit_data, url]
+                                # url_list_output.append(url)
+                            # caunt += 1
 
 
                         except Exception as a:
                             print(a)
+                    except:
+                        print('статья уже есть')
+                # ******************************************************************************************************************************************
 
-                    # ******************************************************************************************************************************************
+                print(output_data)
 
-
-
-
-                    print(output_data)
-
-
-                    # типа проверка статьи
+        time.sleep(random.randrange(3, 5))
 
 
-
-
-
-
-                # exit()
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
 
 
 
-
-
-        except Exception as e:
-            print(e, url, caunt)
-
-        else:
-            return resp
-        return
-
-
-    async def fetch_async(url_lists):
-        tasks = []
-        async with ClientSession() as session:
-            for url, caint, data_master_scan_in in url_lists:
-                # print(url)
-                task = asyncio.ensure_future(fetch_url_data(session, url, caint, data_master_scan_in))
-                tasks.append(task)
-            responses = await asyncio.gather(*tasks)
-        return responses
-
-
-
-
-
-
-    def pars(data_master_scan_in, data_time=(time.time())):
-
-        # ******************************************************************************************************************************************
-        # moderator меняем на название сайта
-
-        url_list_output = []
-        output_data = []
-
+def pars_one(data_master_scan_in, data_time=(time.time())):
+    try:
+        data_time2 = time.localtime(data_time + 24*60*60)
         data_time = time.localtime(data_time)
         print(data_time)
         print(data_time[0])
         print(data_time)
+        url_list_out = []
+        driver = webdriver.Firefox(
+            executable_path=fr"{os.getcwd()}/geckodriver",
+            options=options
+        )
 
-        timer = time.time()
-        urls_list = []
-        caunt = 0
+        # "C:\\users\\selenium_python\\chromedriver\\chromedriver.exe"
+        # r"C:\users\selenium_python\chromedriver\chromedriver.exe"
 
+        driver.get(url='https://modern.az/az')
+        time.sleep(1)
+        caunt_l = 0
 
-        # ******************************************************************************************************************************************
-        # Тут нормальный парсинг, нужно достать ссылки на новости
-
-        # УСЛОВНО РАЗВЕКАТЬСЯ МОЖНО ВОТ ТУТ ↓
-        headers = {'Accept': '*/*', 'Connection': 'keep-alive',
-                   'User-Agent': f'{us.random}',
-                   'Cache-Control': 'max-age=0', 'DNT': '1', 'Upgrade-Insecure-Requests': '1'}
-        # url = f'https://moderator.az/all/?date={data_time[0]}-{data_time[1]}-{data_time[2]}&page={i}'
-        url = f'https://modern.az/az/news/date/{data_time[0]}-{data_time[1]}-{data_time[2]}/page/1'
-        print(url)
-
-        req = requests.get(url, headers=headers)
-
-        src = req.text
-        # print(src)
-        soup = BeautifulSoup(src, 'html.parser')
-
-        page = soup.find(class_='news_divs clearfix')
-        if page.find('a') != None:
-            urls_list_local = []
-            for url_n in page.find_all('a'):
-                if urls_list.count(url_n.get('href')) == 0:
-                    urls_list.append([url_n.get('href'), caunt, data_master_scan_in])
-                    urls_list_local.append([url_n.get('href'), caunt, data_master_scan_in])
-                    caunt += 1  # Это нужно оставить, так как по нему создаются файлы txt
-                # break
-            time.sleep(1)
-            loop = asyncio.get_event_loop()
-            future = asyncio.ensure_future(fetch_async(urls_list_local))
-            loop.run_until_complete(future)
-            time.sleep(1)
-
-        for i in range(2, int(str(soup.find(class_='pagination').find_all('li')[-1].find('a').get('href')).split('/')[-2]) + 1):
-            headers = {'Accept': '*/*', 'Connection': 'keep-alive',
-                       'User-Agent': f'{us.random}',
-                       'Cache-Control': 'max-age=0', 'DNT': '1', 'Upgrade-Insecure-Requests': '1'}
-            # url = f'https://moderator.az/all/?date={data_time[0]}-{data_time[1]}-{data_time[2]}&page={i}'
-            url = f'https://modern.az/az/news/date/{data_time[0]}-{data_time[1]}-{data_time[2]}/page/{i}/'
-            print(url)
-
-            req = requests.get(url, headers=headers)
-
-            src = req.text
-            # print(src)
-            soup = BeautifulSoup(src, 'html.parser')
-
-            page = soup.find(class_='news_divs clearfix')
-            if page.find('a') != None:
-                urls_list_local = []
-                for url_n in page.find_all('a'):
-                    if urls_list.count(url_n.get('href')) == 0:
-                        urls_list.append([url_n.get('href'), caunt, data_master_scan_in])
-                        urls_list_local.append([url_n.get('href'), caunt, data_master_scan_in])
-                        caunt += 1  # Это нужно оставить, так как по нему создаются файлы txt
-                    # break
-                time.sleep(2)
-                loop = asyncio.get_event_loop()
-                future = asyncio.ensure_future(fetch_async(urls_list_local))
-                loop.run_until_complete(future)
-                time.sleep(0.5)
-            else:
-                break
+        # src = driver.page_source
+        len_list = 0
+        time.sleep(2)
+        for i in range(1, 1000):
+            url = f'https://modern.az/az/all/date/{data_time[0]}-{data_time[1]}-{data_time[2]}?page={i}'
+            driver.get(url=url)
+            # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(0.5)
+            src = driver.page_source
+            print(len(src), len_list)
+            # print(data_master_scan_in)
 
 
-            # print(urls_list)
+            try:
+                src = driver.page_source
+                soup = BeautifulSoup(src, 'html.parser')
 
-        print(urls_list)
 
-        # УСЛОВНО РАЗВЕКАТЬСЯ МОЖНО ВОТ ТУТ ↑
+                print(soup.find(id = 'divLoadMore'))
+                for data in soup.find(class_='news_divs infinite clearfix').find_all(class_='news_links inf-item clearfix'):
+                    print(data.find('href'))
 
-        # time.sleep(2)
-        # loop = asyncio.get_event_loop()
-        # future = asyncio.ensure_future(fetch_async(urls_list))
-        # loop.run_until_complete(future)
+                    ur = 'https:' + data.get('href')
+                    print(ur)
+                    url_list_out.append([ur, caunt_l, data_master_scan_in])
+                    caunt_l += 1
 
-        print(time.time() - timer)
+                print(url_list_out)
+                if soup.find(class_='news_divs infinite clearfix').find_all(class_='news_links inf-item clearfix') == []:
+                    break
+
+            except:
+                print('NO')
+
+
+        return url_list_out
+
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
+
+
+def parsing(data_master_scan_in, data_time=(time.time()), process_count = 1):
+
+    url_list_output = []
+
+    with open(f'files/Modern.az/123.txt', 'w', encoding='utf-8') as file:
+        file.write('')
+
+
+    # exit()
+
+    # process_count = int(input("Enter the number of processes: "))
+    #process_count = 6
+
+
+    urls_list = list(func_chunks_generators(pars_one(data_master_scan_in, data_time), process_count))
+    print(urls_list)
+
+    p = Pool(processes=process_count)
+    p.map(get_data, urls_list)
 
 
 
+    out_data_list = []
+
+    for file_l in os.listdir('files/Modern.az'):
+        print(file_l)
+
+        if file_l != '123.txt':
+            with open(f'files/Modern.az/{file_l}', 'r', encoding='utf-8') as file:
+                url = file.readline().replace('\n', '')
+                file.readline()
+
+                txt = file.read()
+            # print(url, txt)
+
+            text_list = txt.lower().split(' ')
+            # print(text_list)
+            # caunt_local = 0
+            exit_data = []
+            for one_line in data_master_scan_in:
+                caunt_local = 0
+                for twe in one_line:
+                    for master_text in text_list:
+                        if fuzz.ratio(master_text, twe) >= 80:
+                            caunt_local += 1
+                            break
+
+                if caunt_local == len(one_line):
+                    exit_data.append(1)
+                else:
+                    exit_data.append(0)
+
+            out_data_list.append([exit_data, url])
+
+    return [[], out_data_list]
 
 
-    pars(data_master_scan_in, data_time)
-
-    return url_list_output, output_data
 
 if __name__ == "__main__":
-    ojr = [['milyon', 'min'], ['ərazisində'], ['məlumatda']]
-    parsing(data_master_scan_in = ojr, data_time=int(time.time() - 24*60*60*50))
-
-
-# Ну потом можно принты почистить, просто не очень прикольно смотреть на пустую консоль
-
+    ojr = [['əlaqəsini', 'həyata'], ['edən'], ['k']]
+    print(parsing(data_master_scan_in = ojr, data_time=int(time.time() - 20*24*60*60), process_count=4))
